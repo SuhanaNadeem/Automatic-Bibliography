7d6a162eedcc460c262b9ef7609dca34329e5e7a
SuhanaNadeem <suhana.nadeemv@gmail.com>
2020-04-12 14:07:50 -0400

    created structure

--- main.js
+++ main.js
@@ -4,4 +4,32 @@
 // function 4: on load, click the CONTINUE button
 // function 5: on load, click the COMPLETE CITATION button
 
+// website: https://www.easybib.com/mla8/website/search-form
+
 let hashtagBtns = document.querySelectorAll('gb_sf gb_tf')
+
+function enterLink() {
+    // alert("This is the first.");
+}
+
+function clickSearch() {
+    // alert("This is the first.");
+}
+
+function clickCite() {
+    // alert("This is the first.");
+}
+
+function clickContinue() {
+    // alert("This is the first.");
+}
+
+function clickComplete() {
+    // alert("This is the first.");
+}
+
+window.onload=enterLink();
+window.onload=clickSearch();
+window.onload=clickCite();
+window.onload=clickContinue();
+window.onload=clickComplete();
