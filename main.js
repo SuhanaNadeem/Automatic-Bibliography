f28f8b9dc3d33da2e28c27d7c71d7ea107eb4ac1
SuhanaNadeem <suhana.nadeemv@gmail.com>
2020-04-12 13:54:34 -0400

    get button class

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-// function 1: on load, enter the first link
-// funcion 2: 
+// function 1: on load, enter the link
+// funcion 2: click the SEARCH button
+// function 3: on load, click the CITE button
+// function 4: on load, click the CONTINUE button
+// function 5: on load, click the COMPLETE CITATION button
+
+let hashtagBtns = document.querySelectorAll('gb_sf gb_tf')
