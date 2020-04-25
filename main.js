42970401d79c271667023489c4b66be0bf7526b2
SuhanaNadeem <suhana.nadeemv@gmail.com>
2020-04-25 08:18:07 -0400

    cleaned up code

--- main.js
+++ main.js
@@ -1,49 +1,5 @@
-// // // function 1: on load, enter the link
-// // // funcion 2: click the SEARCH button
-// // // function 3: on load, click the CITE button
-// // // function 4: on load, click the CONTINUE button
-// // // function 5: on load, click the COMPLETE CITATION button
-
-// // // website: https://www.easybib.com/mla8/website/search-form
-
-// // let toCite = "https://www.khanacademy.org/science/physics/forces-newtons-laws"
-
-// // function clickBtn(className) {
-// //     let hashtagBtns = document.querySelectorAll(className)
-// //     hashtagBtns.forEach(btn => btn.click())
-// // }
-
-// // // WORK ON ENTERING LINK FROM TIS FUNCTION, and
-// // // then clicking search
-// // function enterLink() {
-
-// //     document.getElementsByClassName(".sc-blIhvV").value = toCite
-// //     clickBtn(searchClassName);
-// // }
-
-// // let searchClassName = ".sc-bYTsla"
-// // let citeClassName = ".sc-gisBJw"
-// // let continueClassName = ".sc-gisBJw"
-// // let completeClassName = "sc-kjoXOD"
-
-// // // clickBtn(searchClassName);
-
-// // //uncomment this search call once link thing works
-// // window.onload = function search() {
-// //     clickBtn(searchClassName);
-// // };
-// // window.onload = function cite() {
-// //     clickBtn(citeClassName);
-// // };
-// // window.onload = function continuee(){
-// //     clickBtn(continueClassName);
-// // };
-// // window.onload = function complete() {
-// //     clickBtn(completeClassName);
-// // };
-
 const {Builder, By, Key, util} = require("selenium-webdriver");
-async function example() {
+async function cite() {
     // let driver = await new Builder().forBrowser("firefox").build();
 
     var webdriver = require('selenium-webdriver');
@@ -74,9 +30,5 @@ async function example() {
 
     // await driver.findElement(By.className(continueClassName)).click();
     // await driver.findElement(By.className(completeClassName)).click();
-
 }
-example();
-
-// // Sources:
-// // - https://www.youtube.com/watch?v=QwymPtk4zWo
+cite();
